d131ee79325d5971b6251ec5acc0cac009054c10
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-03-17 19:26:30 +0100

    Refactor making events with session DataFrame

--- src/main/scala/com/utils/PurchasesAttributionProjection.scala
+++ src/main/scala/com/utils/PurchasesAttributionProjection.scala
@@ -1,8 +1,6 @@
 package com.utils
 
 import com.utils.models.Event
-import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{last, when}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object PurchasesAttributionProjection {
@@ -10,17 +8,7 @@ object PurchasesAttributionProjection {
   def viaPlainSparkSQL(events: DataFrame, purchases: DataFrame)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
 
-    val windowSpec = Window
-      .partitionBy($"userId")
-      .orderBy($"eventTime")
-      .rangeBetween(Window.unboundedPreceding, Window.currentRow)
-
-    val eventsWithSessionId = events
-      .withColumn("tmpSessionId", when($"eventType" === "app_open", $"eventId"))
-      .withColumn("sessionId", last($"tmpSessionId", ignoreNulls = true).over(windowSpec))
-      .drop("tmpSessionId")
-      .withColumn("campaignId", last($"attributes.campaign_id", ignoreNulls = true).over(windowSpec))
-      .withColumn("channelIid", last($"attributes.channel_id", ignoreNulls = true).over(windowSpec))
+    val eventsWithSessionId = EventsToEventsWithSession.convert(events)
 
     eventsWithSessionId
       .where($"eventType" === "purchase")
